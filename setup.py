bb1a1de7416d3fbd4a531e32dea210cfc6597bc4
Matt Cowan <cowan@bnl.gov>
2014-11-21 11:10:32 -0500

    fixed up version #s

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def read(*names, **kwargs):
 
 setup(
     name="sampleMangler",
-    version="0.1.0",
+    version="0.1.1",
     license="BSD",
     description="Adapter layer between sampleManager and legacy api.",
     long_description="%s\n%s" % (read("README.rst"), re.sub(":obj:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst"))),
